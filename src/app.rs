75155ea904ba9e72d6a690ccd795096c1dbbdaec
AbdullahElsheshtawy <abdellahontop@gmail.com>
2024-11-25 17:17:10 +0300

    Removed in code validation layer enabling
    Because vkconfig is much more ergonomic than enabling and disabling it
    depending on if developer mode and all that

--- src/app.rs
+++ src/app.rs
@@ -15,8 +15,6 @@ mod frame;
 pub struct App {
     entry: ash::Entry,
     instance: ash::Instance,
-    debug_instance: Option<ash::ext::debug_utils::Instance>,
-    debug_messenger: Option<vk::DebugUtilsMessengerEXT>,
     physical_device: vk::PhysicalDevice,
     device: ash::Device,
     surface: Surface,
@@ -26,67 +24,27 @@ pub struct App {
 }
 
 impl App {
-    pub const VALIDATION: bool = true;
     pub const FRAMES_IN_FLIGHT: u32 = 2;
-    pub const LAYERS: [&CStr; 1] =
-        [unsafe { CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") }];
     pub const REQUIRED_EXTENSIONS: [*const i8; 1] = [ash::khr::swapchain::NAME.as_ptr()];
 
     pub fn new(window: winit::window::Window) -> Result<Self> {
         let entry = unsafe { ash::Entry::load() }?;
         let app_info = vk::ApplicationInfo::default().api_version(vk::API_VERSION_1_3);
-        let layer_names: Vec<*const i8> = if Self::VALIDATION {
-            Self::LAYERS.iter().map(|name| name.as_ptr()).collect()
-        } else {
-            vec![]
-        };
 
-        let debug_extensions = ash::ext::debug_utils::NAME;
-        let mut extension_names: Vec<&CStr> =
+        let extension_names: Vec<&CStr> =
             ash_window::enumerate_required_extensions(window.display_handle()?.as_raw())?
                 .iter()
                 .map(|ext| unsafe { CStr::from_ptr(*ext) })
                 .collect::<Vec<&CStr>>();
-        if Self::VALIDATION {
-            extension_names.push(debug_extensions);
-        }
         let names: Vec<*const i8> = extension_names.iter().map(|name| name.as_ptr()).collect();
 
         let instance_info = vk::InstanceCreateInfo {
             p_application_info: &app_info,
-            enabled_layer_count: layer_names.len() as u32,
-            pp_enabled_layer_names: layer_names.as_ptr().cast(),
             enabled_extension_count: names.len() as u32,
             pp_enabled_extension_names: names.as_ptr(),
             ..Default::default()
         };
         let instance = unsafe { entry.create_instance(&instance_info, None) }?;
-
-        let (debug_instance, debug_messenger) = if Self::VALIDATION {
-            let debug_instance = Some(ash::ext::debug_utils::Instance::new(&entry, &instance));
-            let debug_info = vk::DebugUtilsMessengerCreateInfoEXT {
-                message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
-                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
-                message_type: vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
-                    | vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
-                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
-                    | vk::DebugUtilsMessageTypeFlagsEXT::DEVICE_ADDRESS_BINDING,
-                pfn_user_callback: Some(Self::debug_callback),
-
-                ..Default::default()
-            };
-            let debug_messenger = Some(unsafe {
-                debug_instance
-                    .clone()
-                    .unwrap()
-                    .create_debug_utils_messenger(&debug_info, None)
-            }?);
-            (debug_instance, debug_messenger)
-        } else {
-            (None, None)
-        };
 
         let physical_device = Self::pick_physical_device(&instance)?;
 
@@ -145,14 +103,12 @@ impl App {
         Ok(Self {
             entry,
             instance,
-            debug_messenger,
-            physical_device,
             device,
             window,
-            debug_instance,
             surface,
             swapchain,
             frames,
+            physical_device,
         })
     }
     fn pick_physical_device(instance: &ash::Instance) -> Result<vk::PhysicalDevice> {
@@ -161,24 +117,6 @@ impl App {
             0 => Err(anyhow!("No GPU???")),
             _ => Ok(devices[0]),
         }
-    }
-    unsafe extern "system" fn debug_callback(
-        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
-        message_type: vk::DebugUtilsMessageTypeFlagsEXT,
-        callback_data_ptr: *const vk::DebugUtilsMessengerCallbackDataEXT,
-        _user_data: *mut std::ffi::c_void,
-    ) -> vk::Bool32 {
-        let level = match message_severity {
-            vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => log::Level::Debug,
-            vk::DebugUtilsMessageSeverityFlagsEXT::INFO => log::Level::Info,
-            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => log::Level::Warn,
-            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => log::Level::Error,
-            _ => log::Level::Warn,
-        };
-        let message = std::ffi::CStr::from_ptr((*callback_data_ptr).p_message);
-        log::log!(level, "[{:?}] {:?}", message_type, message);
-
-        vk::FALSE
     }
 
     fn find_queue_family_indicies(
@@ -254,13 +192,6 @@ impl Drop for App {
         unsafe {
             for view in &self.swapchain.image_views {
                 self.device.destroy_image_view(*view, None);
-            }
-
-            if self.debug_instance.is_some() {
-                self.debug_instance
-                    .as_ref()
-                    .unwrap()
-                    .destroy_debug_utils_messenger(self.debug_messenger.unwrap(), None);
             }
 
             for frame in &self.frames {
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
 fn main() -> Result<()> {
-    env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "trace"));
     let event_loop = EventLoop::new()?;
     let window = WindowBuilder::new()
         .with_inner_size(PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
